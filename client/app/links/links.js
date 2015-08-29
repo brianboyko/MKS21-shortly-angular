dead834b6046a73928d34b2370ecd28992d47491
Brian Boyko <brian.boyko@gmail.com>
2015-08-29 17:03:31 -0500

    first commit, 500 server error

---
+++ client/app/links/links.js
@@ -0,0 +1,16 @@
+angular.module('shortly.links', [])
+
+.controller('LinksController', function ($scope, Links) {
+  $scope.data = {};
+  $scope.getLinks = function (){
+    Links.getAll()
+      .then(function(links){
+        $scope.data.links = links;
+      })
+      .catch(function(error){
+        console.error(error);
+      })
+  };
+  $scope.getLinks();
+  $scope.name = "LinksController"
+});
